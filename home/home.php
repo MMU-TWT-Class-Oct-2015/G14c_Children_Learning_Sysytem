584400a50353bd4f6a059144aaddadf7e9777ba4
ramadhan007 <1141123667@student.mmu.edu.my>
2016-02-12 22:36:24 +0800

    home page
    Home

---
+++ home/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<?php include("../includes/header.php") ?>
+
+
+
+<div id="body">
+<div id="home-center">
+
+	<p>Today's kids are more technologically savvy than any other generation of children. From a very young age, kids interact with technology and learn valuable skills that will serve them throughout the course of their lives. Many kids gain their first experience with technology by playing with learning systems. These unique systems are designed to engage kids while building essential academic and social skills. Most education experts now agree that children stand to gain many benefits from playing with learning systems</p>
+	<img src="../image/children.jpg">
+
+	<p>
+		<b>1. Learning Systems Engage Children in Fun, Educational Activities</b><br>
+		Given the prevalence of hi-tech devices in today's world, it can be difficult to engage children in educational activities. Kids expect school and learning activities to be as fun and interesting as the games that they play on various electronic devices. Parents who wish to engage children while offering them good learning opportunities can choose learning systems. Most of these systems look like real computers, cell phones, or tablets. They allow for a high level of interaction while encouraging children to learn and repeat new skills. Keep in mind that learning systems can also help children who have learning disabilities. These systems require students to exercise their hand-eye coordination, tactile skills, reading skills, and speech skills. They make learning non-threatening for children who have struggled in school and are reluctant to participate in activities unless they are receiving one-on-one support. Parents of children with special needs should talk to their teachers to find out what learning systems are used in the classroom. Using the same type of system at home will reinforce learning.
+	</p>
+	<br>
+	<p>
+		<b>2. Learning Systems Help Kids Develop Interests</b><br>
+		Systems are designed to engage kids by presenting them with information about things in which they are already interested. They can also help children develop new interests by presenting thematic information in an engaging, exciting fashion. Today's parents can easily give their kids access to a multitude of hobbies and interests by buying assorted cartridges for learning systems.
+	</p>
+	<br>
+	<p>
+		<b>3. Learning Systems Keep Kids Focused and Occupied</b><br>
+		All children lose their focus from time to time. While there is no way to keep kids focused on one task for every waking hour of the day, learning systems do encourage kids to stay occupied. They allow children to complete straightforward tasks and show off their knowledge to others. Keeping kids engaged is key to ensuring that they will repeat lessons and retain essential information. Of course, many parents simply appreciate that learning systems provide them with a bit of down time after a busy day.
+	</p>
+	<br>
+	<p>
+		<b>Conclusion</b><br>
+		Learning systems are interactive educational tools that almost all children enjoy using. Whether in the home or at school, these systems are effective in engaging children in learning activities and reinforcing existing skills. Parents and teachers who want to give children a head start in schools are well served by purchasing these innovative, hi-tech systems for kids.
+	</p>
+</div>
+
+
+
+<?php include("../includes/footer.php") ?>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Welcome To children learning system</title>
+<script language="JavaScript">
+location.href="home/home.php";
+</script>
+</head>
+
+<body>
+</body>
+</html>
