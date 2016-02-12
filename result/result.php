f76eb438de1364649114eb1056725ad727817a0b
ashrafulamin42 <1131121290@student.mmu.edu.my>
2016-02-12 22:58:16 +0800

    Result Page
    Result

---
+++ result/result.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<?php include("../includes/header.php") ?>
+<div id="body">
+<div id="home-center">
+<h2>This is Result</h2>
+
+
+
+</div>
+<?php include("../includes/footer.php") ?>
+</html>
