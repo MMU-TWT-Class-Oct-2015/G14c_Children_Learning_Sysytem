19b887e02e86d06a84f1020cfa1eda871bb3a516
ashrafulamin42 <1131121290@student.mmu.edu.my>
2016-02-14 14:41:13 +0800

    index
    index

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Welcome To children learning system</title>
+<script language="JavaScript">
+location.href="tutorial/tutorial.php";
+</script>
+</head>
+
+<body>
+</body>
+</html>
