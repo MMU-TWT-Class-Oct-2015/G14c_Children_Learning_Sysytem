1585295b81e769d8f2c5c5af0c7fd9e6b5481b18
ashrafulamin42 <1131121290@student.mmu.edu.my>
2016-02-14 21:58:08 +0800

    math quiz
    math quiz

---
+++ play/math.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<?php include("../includes/header.php") ?>
+<div id="body">
+<div id="home-center">
+<h2>This is math</h2>
+
+1 + 2 = 7, Proof
+
+</div>
+<?php include("../includes/footer.php") ?>
+</html>
