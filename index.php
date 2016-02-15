ed8e02aff5ae44c38c15f20e594ffdf33fa6a083
adrinechen <adrinechen@gmail.com>
2016-02-15 15:02:34 +0800

    index

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Welcome To children learning system</title>
+<script language="JavaScript">
+location.href="register/login.php";
+</script>
+</head>
+
+<body>
+</body>
+</html>
